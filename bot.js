7c858cbe7fcf13464a5b56a9113d53b43b8f988a
Rejectt <45071435+Rejectt@users.noreply.github.com>
2018-12-08 03:43:09 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ client.on("message", (message) => {
     const embed = new Discord.RichEmbed()
     .setTitle(`:mailbox_with_mail: Vulnix Help`)
     .setColor(0xCF40FA)
-    .setDescription(`Hello! I'm Vulnix, the Discord bot for super cool support ticket stuff and more! Here are my commands:`)
+    .setDescription(`مرحبا ! انا تيكيت بوت , ارحب بك , ان كنت تريد اوامري هذه هي :`)
     .addField(`Tickets`, `[${prefix}new]() > Opens up a new ticket and tags the Support Team\n[${prefix}close]() > Closes a ticket that has been resolved or been opened by accident`)
     .addField(`Other`, `[${prefix}help]() > Shows you this help menu your reading\n[${prefix}ping]() > Pings the bot to see how long it takes to react\n[${prefix}about]() > Tells you all about Vulnix`)
     message.channel.send({ embed: embed });
@@ -29,8 +29,8 @@ client.on("message", (message) => {
 
 if (message.content.toLowerCase().startsWith(prefix + `new`)) {
     const reason = message.content.split(" ").slice(1).join(" ");
-    if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
-    if (message.guild.channels.exists("name", "ticket-" + message.author.id)) return message.channel.send(`You already have a ticket open.`);
+    if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`هذا السيرفر لا يملك رتبه \`Support Team\`  لهذا لا يمكنك فتح تيكيت.\n نرجو انشاء ترتبه Support Team حتي يمكنني انشاء التذاكر.`);
+    if (message.guild.channels.exists("name", "ticket-" + message.author.id)) return message.channel.send(`بلفعل انت تملك تيكيت , لا يمكنك انشاء اكثر من واحده .`);
     message.guild.createChannel(`ticket-${message.author.id}`, "text").then(c => {
         let role = message.guild.roles.find("name", "Support Team");
         let role2 = message.guild.roles.find("name", "@everyone");
@@ -46,18 +46,18 @@ if (message.content.toLowerCase().startsWith(prefix + `new`)) {
             SEND_MESSAGES: true,
             READ_MESSAGES: true
         });
-        message.channel.send(`:white_check_mark: Your ticket has been created, #${c.name}.`);
+        message.channel.send(`:white_check_mark: تم انشاء التيكيت الخاص بك وهو بأسم , #${c.name}.`);
         const embed = new Discord.RichEmbed()
         .setColor(0xCF40FA)
-        .addField(`Hey ${message.author.username}!`, `Please try explain why you opened this ticket with as much detail as possible. Our **Support Team** will be here soon to help.`)
+        .addField(`Hey ${message.author.username}!`, `مرحبا , يجب ان تشرح لنا ماهي مشكلتك وسيكون فريق الدعم موجودا هنا لمساعدتك , شكرا .`)
         .setTimestamp();
         c.send({ embed: embed });
     }).catch(console.error);
 }
 if (message.content.toLowerCase().startsWith(prefix + `close`)) {
-    if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
+    if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`لا يمكنك ان تقفل تيكيت خارج القناه الخاصه به.`);
 
-    message.channel.send(`Are you sure? Once confirmed, you cannot reverse this action!\nTo confirm, type \`-confirm\`. This will time out in 10 seconds and be cancelled.`)
+    message.channel.send(`هل انت متاكد من انك تريد غلق هذا التيكيت ؟ \nTo لنتاكد , قم بكتابه  \`-confirm\`. سيتم قفل هذا الفعل في غضوان 10 ثواني .`)
     .then((m) => {
       message.channel.awaitMessages(response => response.content === '-confirm', {
         max: 1,
